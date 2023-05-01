1ebd2547f9612a54955b89678146bc32f76fbe5a
Carl Lundin <carllundin55@gmail.com>
2023-04-30 17:19:49 -0700

    Added configuration.

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,12 @@
 use std::net::TcpListener;
+use zero2prod::configuration::get_config;
+use zero2prod::startup::run;
 
 use anyhow::Result;
 
 #[tokio::main]
 async fn main() -> Result<()> {
-    let listener = TcpListener::bind("localhost:0")?;
-    zero2prod::startup::run(listener).await
+    let config = dbg!(get_config())?;
+    let listener = TcpListener::bind(format!("localhost:{}", config.application_port))?;
+    run(listener).await
 }
